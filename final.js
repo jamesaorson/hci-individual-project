291665a9fa2c6df114b6afe7af171d1187e90af4
James Orson <jamesaorson@gmail.com>
2024-11-02 05:01:37 -0700

    Finish dynamic interface

--- final.js
+++ final.js
@@ -1,34 +1,54 @@
+const username = `<pre class="username"><a href="https://x.com/contextdogs/">@contextdogs</a></pre>`;
+
 function capitalize(text) {
     console.log(text);
     console.log(String(text).charAt(0).toUpperCase() + String(text).slice(1));
     return String(text).charAt(0).toUpperCase() + String(text).slice(1);
 }
 
-function drawSummary() {
-    const isFollowing = document.getElementById("follow-input").checked;
+function drawSummary(event) {
+    const followInput = document.getElementById("follow-input");
+    const blockInput = document.getElementById("block-input");
+
+    if (followInput.checked && blockInput.checked) {
+        followInput.checked = false;
+        if (event.srcElement.id === "follow-input") {
+            return false;
+        }
+    }
+
+    const isFollowing = followInput.checked;
     const isMuted = document.getElementById("mute-input").checked;
-    const isBlocked = document.getElementById("block-input").checked;
+    const isBlocked = blockInput.checked;
+    followInput.disabled = isBlocked;
 
     let you = [];
     let them = [];
     if (isFollowing) {
-        you.push("can direct message them");
-        you.push("will see their posts more frequently");
-        them.push("can direct message me")
+        you.push(`can direct message ${username}`);
+        if (!isMuted) {
+            you.push(`will more frequently see ${username}'s activity on your feed`);
+        }
+        them.push("can direct message you")
+    } else if (isBlocked) {
+        if (!you.includes(`cannot direct message ${username}`)) {
+            you.push(`cannot direct message ${username}`);
+            them.push("cannot direct message you");
+        }
+        you.push(`will not see ${username}'s activity on your feed`);
+        you.push(`can see ${username}'s activity by going directly to their profile`);
+        them.push("can still see your activity");
     } else {
-        you.push("have access to direct messaging");
-        them.push("have access to direct messaging");
+        you.push(`cannot direct message ${username}`);
+        them.push("cannot direct message you");
     }
     if (isMuted) {
-        you.push("will not see their activity on your feed");
-        them.push("will not appear on your feed");
-    } else {
-
-    }
-    if (isBlocked) {
-        // summary.push("blocked");
-    } else {
-
+        if (isFollowing) {
+            you.push("will not receive notifications for direct messages")
+        }
+        if (!you.includes(`will not see ${username}'s activity on your feed`)) {
+            you.push(`will not see ${username}'s activity on your feed`);
+        }
     }
 
     for (const [pro, proId] of [[you, "you-text"], [them, "them-text"]]) {
